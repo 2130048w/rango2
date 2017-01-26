e314dceda64bb0f965ff15025006b3169f65ca9a
unknown <2130048w@BO706-05.ad.dcs.gla.ac.uk>
2017-01-26 13:18:21 +0000

    commit 26 Jan

---
+++ rango/views.py
@@ -0,0 +1,31 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from rango.models import Category, Page
+
+def index(request):
+    # query the database for a list of ALL categories currently stored
+    # order the categories by no. likes in descending order
+    # retrieve the top 5 only - or all less than 5
+    # place the list in our context_dict dictionary
+    # that will be passed to the template engine
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    #Render the response and send it back
+    return render(request,'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': "Carla Whiteford"}
+    return render(request,'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict={}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
