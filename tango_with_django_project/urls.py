ad4e22f52bed9e62bb201727bc42d38978719f76
Carla Whiteford <Carla Whiteford>
2017-02-09 18:30:07 +0000

    fixed errors up to chp10

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rango import views
 
@@ -12,4 +14,4 @@ urlpatterns = [
     # rango/ to be handled by the
     # rango application
 url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
